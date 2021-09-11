752ce8812e3d57f44ac921df10395eb7518d4d5e
jibranabsarulislam <jga26@njit.edu>
2021-09-11 01:12:42 -0400

    create APIreader and testcases with output

--- Feed/testcases.py
+++ Feed/testcases.py
@@ -1,1 +1,17 @@
-# hello pennapps
+import APIReader
+
+baking_soda = APIReader.APIReader("baking_soda")
+canned_hash = APIReader.APIReader("hash")
+canned_peas = APIReader.APIReader("peas")
+coffee_container = APIReader.APIReader("coffee")
+frosted_cereal = APIReader.APIReader("frosted_flakes")
+heavy_cream = APIReader.APIReader("cream")
+olive_oil = APIReader.APIReader("olive_oil")
+sriracha = APIReader.APIReader("sriracha")
+tomato_sauce = APIReader.APIReader("tomato_sauce")
+whipped_cream = APIReader.APIReader("whipped")
+
+file_vars = dir()
+for v in file_vars:
+    print(eval(v))
+
