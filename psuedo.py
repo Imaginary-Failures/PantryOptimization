597f5395aebf6bb3606cb6dda85402573f3f7656
jibranabsarulislam <jga26@njit.edu>
2021-09-11 14:25:25 -0400

    pseudo work

---
+++ psuedo.py
@@ -0,0 +1,10 @@
+# Create and sort a list of Foodstuff objects based on length * depth
+
+
+# determine how things are getting added to the stack
+# While shelf != empty:
+    # while currStack.height < currStack.max_height:
+        #if
+        #currStack.push
+
+
